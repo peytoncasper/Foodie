9b31c92daf2d784b5c4e0739e7762904ea637d35
Peyton casper <peyton.casper@datastax.com>
2015-09-19 00:56:46 -0500

    Fixes for FoodieWeb, additional instructions.

--- FoodieWeb/FoodieWeb.py
+++ FoodieWeb/FoodieWeb.py
@@ -9,6 +9,9 @@ app.config.from_pyfile('application.cfg')
 app.register_blueprint(web)
 
 def start():
+
+
+
     create_cassandra_session(app.config["DSE_CLUSTER"].split(','), app.config["DSE_KEYSPACE"])
     app.run(host='0.0.0.0',
             port=5000,
